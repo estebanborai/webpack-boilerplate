be067430416fa9b502dd37c76d08da2de8f55c2a
Esteban Borai <estebanborai@gmail.com>
2020-01-27 23:51:50 -0300

    Update dependencies. Enhance setup

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:latest
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN yarn
-COPY . .
-EXPOSE 8080
-CMD [ "yarn", "start" ]
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,96 @@
-/* eslint-disable */
-
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'bundle')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'assets/[name].[ext]',
-						}
-					}
-				]
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: 'file-loader',
-				options: {
-					name: 'assets/[name].[ext]'
-				}
-			}
-		]
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'src'),
-		compress: true,
-		port: 8080
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html'
-		}),
-    new HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin()
-	],
-	resolve: {
-		extensions: ['.js']
-	}
-};
+module.exports = (env, args) => {
+  const isDev = args.mode === 'development';
+  const isProd = args.mode === 'production';
+
+  const config = {
+    module: {}
+  };
+
+  config.entry = './src/main.js';
+
+  config.output = {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  }
+
+  config.module.rules = [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css$/,
+      use:  [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }
+  ];
+
+  config.plugins = [
+    new MiniCssExtractPlugin({
+      filename: "style.[hash].css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './public/index.ejs',
+      filename: 'index.html',
+      title: 'webpack-boilerplate'
+    })
+  ]
+  
+  if (isDev) {
+    config.devServer = {
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+      inline: true,
+      compress: true,
+      port: 8080,
+      historyApiFallback: true,
+      watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 1500,
+      },
+      stats: {
+        colors: true,
+        hash: false,
+        version: false,
+        timings: false,
+        assets: false,
+        chunks: false,
+        modules: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: true,
+        errorDetails: true,
+        warnings: true,
+        publicPath: false
+      }
+    };
+  }
+
+  if (isProd) {
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+
+  return config;
+}
